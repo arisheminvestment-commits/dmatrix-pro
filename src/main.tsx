d61df4336dcbd731daf51ca531cca99f5c6b7522
arisheminvestment-commits <arisheminvestment@gmail.com>
2026-07-25 20:50:12 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,50 +1,15 @@
 import React from 'react';
-import { observer } from 'mobx-react-lite';
-import { NavLink } from 'react-router-dom';
-import classNames from 'classnames';
+import ReactDOM from 'react-dom/client';
+import { AuthWrapper } from './app/AuthWrapper';
+import './app/app-root.scss';
 
-export const MenuItems = observer(() => {
-    return (
-        <div className='app-header__menu-items'>
-            <NavLink
-                to='/dashboard'
-                className={({ isActive }) => classNames('app-header__menu-item', { 'app-header__menu-item--active': isActive })}
-            >
-                Dashboard
-            </NavLink>
-            <NavLink
-                to='/bot-builder'
-                className={({ isActive }) => classNames('app-header__menu-item', { 'app-header__menu-item--active': isActive })}
-            >
-                Bot Builder
-            </NavLink>
-            <NavLink
-                to='/charts'
-                className={({ isActive }) => classNames('app-header__menu-item', { 'app-header__menu-item--active': isActive })}
-            >
-                Charts
-            </NavLink>
-            <NavLink
-                to='/tutorials'
-                className={({ isActive }) => classNames('app-header__menu-item', { 'app-header__menu-item--active': isActive })}
-            >
-                Tutorials
-            </NavLink>
-        </div>
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <React.StrictMode>
+            <AuthWrapper />
+        </React.StrictMode>
     );
-});
-
-export const TradershubLink = observer(() => {
-    return null;
-});
-
-// Define type including the component namespace
-type MenuItemsType = typeof MenuItems & {
-    TradershubLink: typeof TradershubLink;
-};
-
-// Assign the static TradershubLink property
-(MenuItems as MenuItemsType).TradershubLink = TradershubLink;
-
-// Default export using the specific type
-export default MenuItems as MenuItemsType;
+}
